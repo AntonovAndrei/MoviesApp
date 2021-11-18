1f3d14f6e5c610bebd48ae6221fb8a6bb34b5e3b
AntonovAndrei <camuy4onkoa4a@yandex.ru>
2021-11-18 23:32:20 +0300

    add ActersController.cs

--- MoviesApp/Controllers/ActersController.cs
+++ MoviesApp/Controllers/ActersController.cs
@@ -1,7 +1,10 @@
 ﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using MoviesApp.Data;
+using MoviesApp.Models;
+using MoviesApp.ViewModels;
 
 namespace MoviesApp.Controllers
 {
@@ -22,10 +25,167 @@ namespace MoviesApp.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            return View(_context.Acters.Select(new => A))
+            return View(_context.Acters.Select(a => new ActerViewModel
+            {
+                Id = a.Id,
+                Name = a.Name,
+                LastName = a.LastName,
+                BirthdayDate = a.BirthdayDate
+            }).ToList());
         }
         
-        
+        // GET: Acter/Details/5
+        [HttpGet]
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
+            var viewModel = _context.Acters
+                .Where(a => a.Id == id)
+                .Select(a => new ActerViewModel
+                {
+                    Id = a.Id,
+                    Name = a.Name,
+                    LastName = a.LastName,
+                    BirthdayDate = a.BirthdayDate
+                }).FirstOrDefault();
+            if (viewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(viewModel);
+        }
+        
+        // GET: Acters/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Name, LastName, BirthdayDate")] InputActerViewModel inputModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Acters.Add(new Acter
+                {
+                    Name = inputModel.Name,
+                    LastName = inputModel.LastName,
+                    BirthdayDate = inputModel.BirthdayDate
+                });
+
+                _context.SaveChanges();
+                return RedirectToAction((nameof(Index)));
+            }
+
+            return View(inputModel);
+        }
+        
+        // GET: Acters/Edit/5
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var editModel = _context.Acters
+                .Where(a => a.Id == id)
+                .Select(a => new EditActerViewModel
+                {
+                    Name = a.Name,
+                    LastName = a.LastName,
+                    BirthdayDate = a.BirthdayDate
+                }).FirstOrDefault();
+            if (editModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(editModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("Name, LastName, BirthdayDate")] EditActerViewModel editModel)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var acter = new Acter
+                    {
+                        Id = id,
+                        Name = editModel.Name,
+                        LastName = editModel.LastName,
+                        BirthdayDate = editModel.BirthdayDate
+                    };
+
+                    _context.Update(acter);
+                    _context.SaveChanges();
+                }
+                catch (DbUpdateException)
+                {
+                    if (!ActerExists(id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(editModel);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var deleteModel = _context.Acters.Where(a => a.Id == id).Select(a => new DeleteActerViewModel
+            {
+                Name = a.Name,
+                LastName = a.LastName,
+                BirthdayDate = a.BirthdayDate
+            }).FirstOrDefault();
+            
+            if (deleteModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(deleteModel);
+        }
+        
+        // POST: Acters/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var acter = _context.Acters.Find(id);
+            _context.Acters.Remove(acter);
+            _context.SaveChanges();
+            _logger.LogError($"Movie with id {acter.Id} has been deleted!");
+            return RedirectToAction(nameof(Index));
+        }
+        private bool ActerExists(int id)
+        {
+            return _context.Acters.Any(a => a.Id == id);
+        }
     }
 }
