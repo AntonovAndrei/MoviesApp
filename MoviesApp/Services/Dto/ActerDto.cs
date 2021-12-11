6d765367103408c5249fef0242ce5a2eb3f1b279
AntonovAndrei <camuy4onkoa4a@yandex.ru>
2021-12-11 16:25:32 +0300

    Add Dto

--- MoviesApp/Services/Dto/ActerDto.cs
+++ MoviesApp/Services/Dto/ActerDto.cs
@@ -1,7 +1,25 @@
-﻿namespace MoviesApp.Services.Dto
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using MoviesApp.Filters;
+
+namespace MoviesApp.Services.Dto
 {
     public class ActerDto
     {
+        public int? Id { get; set; }
         
+        [Required]
+        [StringLength(32,ErrorMessage = "Name length can't be more than 32.")]
+        [ActorName]
+        public string Name { get; set; }
+        
+        [Required]
+        [StringLength(32,ErrorMessage = "Last name length can't be more than 32.")]
+        [ActorName]
+        public string LastName { get; set; }
+        
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime BirthdayDate { get; set; }
     }
 }
---
+++ MoviesApp/ViewModels/AutoMapperProfiles/ActerViewModelProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MoviesApp.Services.Dto;
+using MoviesApp.ViewModels;
+
+namespace MoviesApp.Models.AutoMapperProfiles
+{
+    public class ActerProfile : Profile
+    {
+        public ActerProfile()
+        {
+            CreateMap<ActerDto, InputActerViewModel>().ReverseMap();
+            CreateMap<ActerDto, DeleteActerViewModel>();
+            CreateMap<ActerDto, EditActerViewModel>().ReverseMap();
+            CreateMap<ActerDto, ActerViewModel>().ReverseMap();
+        }
+    }
+}
